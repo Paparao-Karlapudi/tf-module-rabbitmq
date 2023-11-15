bf020624fced49333e8d87a3732b818dd6da9448
pappik <pappik@gmail.com>
2023-11-15 01:24:17 -0500

    documentdb

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "engine_version" {}
+variable "engine_type" {}
+variable "host_instance_type" {}
+variable "deployment_mode" {}
+
